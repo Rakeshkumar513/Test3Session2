313351daa5fa1972142a6cf521581d4ef4399e8e
bachu rakesh <93091226+Rakeshkumar513@users.noreply.github.com>
2022-03-08 04:23:08 +0000

    DONE

--- p3final.c
+++ p3final.c
@@ -1,48 +1,23 @@
-/*
-Write a program find whether a given number is a prime number.
-int input_number();
-int is_prime(int n);
-void output(int n, int is_prime);
-*/
-
-#include <stdio.h>
-#include <math.h>
-int input()
+#include<stdio.h>
+void input(int *a,int *b)
 {
-  int n;
-  printf("Enter the number\n");
-  scanf("%d",&n);
-  return n;
+  printf("enter the values of a,b\n");
+  scanf("%d%d",a,b);
 }
-
-int isprime(int n)
+int add(int a,int b, int sum)
 {
-  if (n==0 || n==1) {
-    return 0
-  }
-  if (n==2) {
-    return 1;
-  }
-  for(int i= 2; i<= sqrt(n);i++)
-  {
-    if(n%i==0)
-      return 0;
-  }
-  return 1;
+  sum=a+b;
+  return sum;
 }
-void output(int n, int isp)
+void output(int *a,int *b,int *sum)
 {
-  if(isp == 0)
-    printf("%d is not a prime number\n",n);
-  else
-    printf("%d is a prime number\n",n)
+  printf("the sum of two numbers = %d+%d = %d",*a,*b,*sum);
 }
-
 int main()
 {
-  int n = input();
-  int isp = isprime(n);
-  output(n,isp);
+  int a,b,sum; 
+  input(&a,&b);
+  sum=add(a,b,sum);
+  output(&a,&b,&sum);
   return 0;
   }
-}
